3796cd5061e47024305e05990e342aa0a0e6bdb6
juan17castillo <juancastillo9910@gmail.com>
2021-10-07 13:06:42 -0500

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsListComponent } from './components/cards-list/cards-list.component';
+import { CardItemComponent } from './components/cards-list/card-item/card-item.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardFormComponent } from './components/card-form/card-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsListComponent,
+    CardItemComponent,
+    HeaderComponent,
+    CardFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
