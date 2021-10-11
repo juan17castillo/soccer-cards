d663b2b44cebd862619a3c0d8fdfa869877cd747
juan17castillo <juancastillo9910@gmail.com>
2021-10-10 20:06:22 -0500

    fix: Destroying component after leaving

--- src/app/components/cards-list/cards-list.component.ts
+++ src/app/components/cards-list/cards-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Player } from 'src/models/Player.model';
 import { SoccerCardsService } from './soccer-cards.service';
@@ -8,18 +8,21 @@ import { SoccerCardsService } from './soccer-cards.service';
   templateUrl: './cards-list.component.html',
   styleUrls: ['./cards-list.component.scss']
 })
-export class CardsListComponent implements OnInit {
+export class CardsListComponent implements OnInit, OnDestroy {
 
   players: Player[];
   private playersSub: Subscription;
 
   constructor(private soccerCardsService: SoccerCardsService) { }
-
+  
   ngOnInit(): void {
     this.players = this.soccerCardsService.getPlayers();
     this.playersSub = this.soccerCardsService.playersEvent.subscribe((players: Player[]) => {
       this.players = players;
     })
   }
-
+  
+  ngOnDestroy(): void {
+    this.playersSub.unsubscribe();
+  }
 }
